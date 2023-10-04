58a25f6eb8045e10cc6505975472de27bfcccbde
jdsouthern <jdsouthern20@gmail.com>
2023-10-04 17:47:29 -0400

    comments on PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,29 +18,29 @@ with open(budgetDataCSV, 'r') as csvFile:
     changesList = []
     greatestIncrease = [initials[0], int(initials[1])]
     greatestDecrease = [initials[0], int(initials[1])]
-
+    #loop through remainder of csv
     for row in csvReader:
-        totalMonths += 1
-        totalProfit += int(row[1])
-        changeFromLast = int(row[1])-previousRow
-        changesList.append(changeFromLast)
-        previousRow = int(row[1])
-        if changeFromLast > greatestIncrease[1]:
+        totalMonths += 1 #count total months
+        totalProfit += int(row[1]) #calc total profit
+        changeFromLast = int(row[1])-previousRow #calc monthly change
+        changesList.append(changeFromLast) #store monthly change
+        previousRow = int(row[1]) #update value
+        if changeFromLast > greatestIncrease[1]: #find greatest increase
             greatestIncrease[0] = row[0]
             greatestIncrease[1] = changeFromLast
-        if changeFromLast < greatestDecrease[1]:
+        if changeFromLast < greatestDecrease[1]: #find greatest decrease
             greatestDecrease[0] = row[0]
             greatestDecrease[1] = changeFromLast
-        
+#formatted terminal output      
 print("Financial Analysis\n")
 print("----------------------------\n")
 print(f"Total Months: {totalMonths}\n")
 print(f"Total: ${totalProfit}\n")
-averageChange = sum(changesList) / len(changesList)
+averageChange = sum(changesList) / len(changesList) #calc average monthly change
 print(f"Average Change: ${averageChange:.2f}\n")
 print(f"Greatest Increase in Profits: {greatestIncrease[0]} (${greatestIncrease[1]})\n")
 print(f"Greatest Decrease in Profits: {greatestDecrease[0]} (${greatestDecrease[1]})\n")
-
+#IO wrapper and formatted txt output
 analysisTXT = os.path.join("analysis", "PyBank Analysis.txt")
 with open(analysisTXT, 'w') as txtFile:
      
